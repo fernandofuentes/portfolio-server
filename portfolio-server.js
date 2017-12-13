44cb2ea2a18936d33ed8f5b565b1980cce520b1f
Fernando Fuentes <fernando.fuentes1113@gmail.com>
2017-12-12 19:41:50 -0600

    init

---
+++ portfolio-server.js
@@ -0,0 +1,35 @@
+// Import HTTP module
+const http = require("http");
+// Define the port to listen on
+// YOU CANNOT LISTEN ON PORT 80 <---- WHY
+const PORT = 8080;
+
+
+
+// Handle incoming requests
+function responseHandler(request, response) {
+  const path = request.url;
+
+  switch (path) {
+    case "/":
+      response.send("You're at root");
+    case "/portfolio":
+      response.end("Mah portfolio");
+    default:
+      response.endZ("404");
+  }
+}
+
+
+const server = http.createServer(responseHandler);
+
+
+
+
+// tell the server to LISTEN to PORT
+server.listen(PORT, function() {
+  console.log("====================================");
+  console.log("I'm listening to PORT");
+  console.error("Visit http:..localhost:${PORT}");
+  console.log("====================================");
+});
